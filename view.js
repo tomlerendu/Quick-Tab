034078fb9d46ff48d218a3c9e52dea9297454fa1
Tom <tom.lerendu@gmail.com>
2013-03-14 23:04:45 +0000

    Added search box clear button

--- view.js
+++ view.js
@@ -1,9 +1,46 @@
 $(document).ready(function()
 {
+	//Creates the tab list
 	generateList();
+	
+	//If something is typed in the search box
+	$('#search > input').bind('keyup', function() {
+		doSearch($(this).val());
+	});
+	
+	//If the clear search box button is pressed
+	$('#search > #clear').bind('mousedown', function(){
+		clearSearch();
+	});
 });
 
-function switchToTab(tabId)
+function clearSearch()
+{
+	//Focus the search box and remove it's
+	$('#search > input').focus().val('');
+	//Hide the clear button
+	$('#clear').css('display','none');
+}
+
+function doSearch(term)
+{
+	// TODO: this bit should be moved
+	if(term == undefined || term == '') $('#clear').css('display','none');
+	else $('#clear').css('display','block');
+	
+	//The term that must be matched
+	var regex = new RegExp('(' + term + ')', 'gi');
+	
+	//Match against each tab
+	$('#tabs > .tab').foreach(function() {
+		if($(this).data('title').match(regex))
+		{
+			
+		} 
+	});
+}
+
+function switchTab(tabId)
 {
 	chrome.tabs.update(
 		tabId,
@@ -13,33 +50,46 @@ function switchToTab(tabId)
 	window.close();
 }
 
-function closeTab(tabId)
+function generateTabView(id, title, url, fav)
 {
-	
+	if (fav == undefined || fav == "") 
+		fav = "images/blank.png";
+			
+	var tabView = $(
+		'<div class="tab">' +
+			'<div class="favicon"><img src="' + fav + '" /></div>' +
+			'<div class="title">' + title + '</div>' +
+		'</div>'
+	)
+	.data('id', id)
+	.data('title', title);
+			
+	$('#tabs').append(tabView);
 }
 
 function generateList()
 {
 	chrome.tabs.getAllInWindow(null, function(tabs)
 	{
+		//Create the HTML for each tab
 		for(var i=0; i<tabs.length; i++)
-		{
-			var tabView = $(
-					'<div class="tab">' +
-						'<div class="favicon"><img src="' + tabs[i].favIconUrl + '" /></div>' +
-						'<div class="title">' + tabs[i].title + '</div>' +
-					'</div>'
-				)
-				.data('id', tabs[i].id)
-				.data('title', tabs[i].title)
-				.data('url', tabs[i].url);
-			
-			$('#tabs').append(tabView);
-		}
+			generateTabView(tabs[i].id, tabs[i].title, tabs[i].url, tabs[i].favIconUrl);
 		
-		$('#tabs').children().bind('click', function()
-		{
-			switchToTab($(this).data('id'));
-		});
+		//Mouse events for each tab
+		$('#tabs').children()
+			//Switch tab
+			.bind('mousedown', function() {
+				switchTab($(this).data('id'));
+			})
+			//Close tab
+			.bind('contextmenu', function() {
+			
+				chrome.tabs.remove($(this).data('id'));
+				$(this).slideUp(100, function() {
+					$(this).remove();
+				});
+				return false;
+				
+			});
 	});
 }
