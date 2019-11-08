a3be6a8a3aae113110baf0cce45bb74f1494f455
Tom Lerendu <tom.lerendu@gmail.com>
2019-11-08 00:43:00 +0000

    Control the tab list with keys

--- src/components/tab-list/tab-list.jsx
+++ src/components/tab-list/tab-list.jsx
@@ -4,14 +4,54 @@ import PropTypes from 'prop-types';
 import { Tab } from '../tab/tab';
 
 export class TabList extends React.Component {
+  state = {
+    currentlySelectedTab: null,
+  };
+
+  mouseEnteredTab(tab) {
+    this.setState({
+      currentlySelectedTab: tab,
+    });
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.keyPressed.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.keyPressed.bind(this));
+  }
+
+  keyPressed(event) {
+    if (!['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+      return;
+    }
+
+    let index = this.props.tabs.indexOf(this.state.currentlySelectedTab);
+
+    if (['ArrowUp', 'ArrowLeft'].includes(event.key)) {
+      index = Math.max(0, index - 1);
+    }
+
+    if (['ArrowDown', 'ArrowRight'].includes(event.key)) {
+      index = Math.min(this.props.tabs.length - 1, index + 1);
+    }
+
+    this.setState({
+      currentlySelectedTab: this.props.tabs[index],
+    });
+  }
+
   render() {
     return (
-      <>
+      <div>
         { this.props.tabs.map(tab => {
-          console.log(tab);
-          return <Tab key={ tab.id } tab={ tab }/>
+          return <Tab key={ tab.id }
+                      tab={ tab }
+                      selected={ this.state.currentlySelectedTab === tab }
+                      onMouseEnter={ () => this.mouseEnteredTab(tab) }/>
         }) }
-      </>
+      </div>
     );
   }
 }
--- src/components/tab/tab.jsx
+++ src/components/tab/tab.jsx
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types';
 export class Tab extends React.Component {
   render() {
     return (
-      <div className={ 'flex p-2 cursor-pointer hover:bg-blue-100' }>
+      <div className={ `flex p-2 cursor-pointer ${ this.props.selected ? 'bg-blue-100' : '' }` }>
         <div className={ 'w-16' }>
-          <img className={ 'w-16 '} src={ this.props.tab.favIconUrl }/>
+          <img className={ 'w-16' } src={ this.props.tab.favIconUrl }/>
         </div>
-        <div className={ 'tab-title' }>{ this.props.tab.title }</div>
+        <div className={ 'w-auto' }>{ this.props.tab.title }</div>
       </div>
     );
   }
@@ -17,4 +17,5 @@ export class Tab extends React.Component {
 
 Tab.propTypes = {
   tab: PropTypes.object,
+  selected: PropTypes.bool,
 };
